fe3520bd2fad62357fb30c1d5a54472c9b3d43e4
Saleh Basalim <156353463+salehmb89@users.noreply.github.com>
2024-03-15 22:57:13 -0500

    Update script.js

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+$(function () {
+    console.log("ready!");
+  
+    $.each(golfGames, (index, data) => {
+      $("#gameRow").append(`
+      <div class="col-sm-5 mx-auto mt-1">
+          <div class="card">
+            <h2 class="card-title"> ${golfGames[index].author} </h2>
+            <div class="card-body"><h3>Game Name: ${golfGames[index].game}</h3></div>
+  
+            <div class="card-footer">
+              <a class="btn btn-dark" href="${golfGames[index].url}" target="_blank">
+              Go have fun!
+              </a>
+            </div>
+  
+          </div>
+        </div>
+        `);
+    });
+  });
