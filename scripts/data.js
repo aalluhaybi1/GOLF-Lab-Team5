8c865253cd3b0855222ff8d54633d93241f32871
Saleh Basalim <156353463+salehmb89@users.noreply.github.com>
2024-03-15 22:57:37 -0500

    Create data.js

---
+++ scripts/data.js
@@ -0,0 +1,151 @@
+const golfGames = [
+    // Existing games...
+  
+    {
+      game: "The Mystical Lucky Eccentric Silly Llama Game!",
+      author: "Michael Hayden Duran",
+      url: "https://github.com/MichaelHaydenDuran/chica-bonita-delta"
+    },
+    {
+      game: "Charlie Lab Doge Game",
+      author: "Michael Hayden Duran",
+      url: "https://github.com/MichaelHaydenDuran/silly-dino-charlie"
+    },
+    {
+      game: "charlie-project-alex",
+      author: "Alex Glenn",
+      url: "https://github.com/pglenn1/charlie-project-alex"
+    },
+    {
+      game: "delta-project-alex",
+      author: "Alex Glenn",
+      url: "https://pglenn1.github.io/delta-project-alex/"
+    },
+    {
+      game: "Charlie-BravoSoMuchWow",
+      author: "Kevin Hernandez",
+      url: "https://hkevin555.github.io/lab-charlie/"
+    },
+    {
+      game: "Spirit Animal Name Game",
+      author: "Kevin Hernandez",
+      url: "https://hkevin555.github.io/DeltaLab/"
+    },
+    {
+      game: "Delta Lab Spirit animal",
+      author: "Marvin De La Cruz",
+      url: "https://marvin-2056.github.io/Delta-CIS-367/"
+    },
+    {
+      game: "nesia-charlie-lab",
+      author: "Rodnesia Goodloe",
+      url: "https://github.com/nesiagoodloe/nesia-charlie-lab"
+    },
+    {
+      game: "currentDeltaLab",
+      author: "Rodnesia Goodloe",
+      url: "https://github.com/nesiagoodloe/currentDeltaLab"
+    },
+    {
+      game: "Charlie",
+      author: "Claire Mitchell",
+      url: "https://cm1803.github.io/charlie/"
+    },
+    {
+      game: "Delta",
+      author: "Claire Mitchell",
+      url: "https://cm1803.github.io/delta/"
+    },
+    {
+      game: "Delta-lab",
+      author: "Saleh Basalim",
+      url: "https://salehmb89.github.io/delta-lab/"
+    },
+    {
+      game: "Charlie-lab",
+      author: "Saleh Basalim",
+      url: "https://salehmb89.github.io/waffle-charlie/"
+    },
+    {
+      game: "Hopping Zone Charlie",
+      author: "Mandy McGinnis",
+      url: "https://github.com/mmandelyn/silver-seal-charlie"
+    },
+    {
+      game: "Spirit Animal Delta",
+      author: "Mandy McGinnis",
+      url: "https://github.com/mmandelyn/pink-panda-delta"
+    },
+    {
+      game: "super-adventure-charlie",
+      author: "Skyler Saunders",
+      url: "https://skysaunders.github.io/super-adventure-charlie/"
+    },
+    {
+      game: "animated-sniffle-foxtrot",
+      author: "Skyler Saunders",
+      url: "https://skysaunders.github.io/animated-sniffle-foxtrot/"
+    },
+    {
+      game: "Glowing Meme Foxtrot",
+      author: "Candice Buford",
+      url: "https://cbuf0rd.github.io/glowing-meme-foxtrot/"
+    },
+    {
+      game: "Glowing Giggle Echo Lab",
+      author: "Candice Buford",
+      url: "https://cbuf0rd.github.io/glowing-giggle-echo-lab/"
+    },
+    {
+      game: "Delta",
+      author: "Mohammed Alrasheidi",
+      url: "https://mohammedalrasheidi.github.io/Delta-lab/"
+    },
+    {
+      game: "Charlie",
+      author: "Mohammed Alrasheidi",
+      url: "https://mohammedalrasheidi.github.io/Charlie/"
+    },
+    {
+      game: "charlie",
+      author: "Grant Hartley",
+      url: "https://github.com/hrantgartley/didactic-waffle-delta"
+    },
+    {
+      game: "delta",
+      author: "Grant Hartley",
+      url: "https://github.com/hrantgartley/didactic-waffle-delta"
+    },
+    {
+      game: "echo",
+      author: "Grant Hartley",
+      url: "https://github.com/hrantgartley/jubilant-bassoon-echo"
+    },
+    {
+      game: "foxtrot",
+      author: "Grant Hartley",
+      url: "https://github.com/hrantgartley/lesser-acorn-foxtrot"
+    },
+    {
+      game: "delta",
+      author: "Ayman Alluhaybi",
+      url: "https://aalluhaybi1.github.io/DELTA-LAB/"
+    },
+    {
+      game: "CHARLIE",
+      author: "Ayman Alluhaybi",
+      url: "https://aalluhaybi1.github.io/CHARLIE_LAB/"
+    },
+    {
+      game: "Charlie",
+      author: "Brayden Bolton",
+      url: "https://github.com/bbolton1/CIS376-Charlie-Brayden"
+    },
+    {
+      game: "Delta",
+      author: "Brayden Bolton",
+      url: "https://github.com/bbolton1/delta-lab-brayden"
+    },
+    // Add more games here as needed
+  ];
+  
